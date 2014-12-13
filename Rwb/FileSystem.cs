9652bd2834cbf815a8ce55dcdcf6d483e8a5cfe6
Raphaël WB <raphael-wb@git>
2014-12-13 20:48:28 +0100

    Add FileSystem abstraction

---
+++ Rwb/FileSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Rwb
+{
+	public class FileSystem : IFileSystem
+	{
+		public static StringComparer PathComparer => StringComparer.OrdinalIgnoreCase;
+
+		public bool FileExists(string path) => File.Exists(path);
+
+		public bool DirectoryExists(string path) => Directory.Exists(path);
+
+		public void CreateDirectory(string path) => Directory.CreateDirectory(path);
+
+		public Stream OpenRead(string path) => File.OpenRead(path);
+
+		public Stream OpenWrite(string path) => File.OpenWrite(path);
+
+		public IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOption) =>
+			Directory.EnumerateFiles(path, searchPattern, searchOption);
+
+		public IEnumerable<string> EnumerateDirectories(string path, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly) =>
+			Directory.EnumerateDirectories(path, searchPattern, searchOption);
+
+		public void Copy(string sourceFileName, string destinationFileName, bool overwrite) =>
+			File.Copy(sourceFileName, destinationFileName, overwrite);
+
+		public async Task CopyAsync(string sourceFileName, string destinationFileName, bool overwrite)
+		{
+			Require.NotEmpty(sourceFileName, nameof(sourceFileName));
+			Require.NotEmpty(destinationFileName, nameof(destinationFileName));
+
+			var fileMode = overwrite ? FileMode.Create : FileMode.CreateNew;
+
+			using (var sourceStream = OpenRead(sourceFileName))
+			using (var destinationStream = File.Open(destinationFileName, fileMode, FileAccess.Write))
+				await sourceStream.CopyToAsync(destinationStream);
+		}
+
+		public void Move(string sourceFileName, string destinationFileName) => File.Move(sourceFileName, destinationFileName);
+
+		public void DeleteFile(string path) => File.Delete(path);
+
+		public void SetAttributes(string path, FileAttributes fileAttributes) => File.SetAttributes(path, fileAttributes);
+
+		public void Encrypt(string path) => File.Encrypt(path);
+
+		public Stream Open(string path, FileMode mode, FileAccess access, FileShare share, FileOptions fileOptions) =>
+			new FileStream(path, mode, access, share, 4096, fileOptions);
+
+		public DateTime GetLastWriteTimeUtc(string path) => File.GetLastWriteTimeUtc(path);
+
+		public void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc)
+		{
+			Require.Utc(lastWriteTimeUtc, nameof(lastWriteTimeUtc));
+			File.SetLastWriteTimeUtc(path, lastWriteTimeUtc);
+		}
+	}
+}
---
+++ Rwb/IFileSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Rwb
+{
+	public interface IFileSystem
+	{
+		bool DirectoryExists(string path);
+		bool FileExists(string path);
+		void CreateDirectory(string path);
+		Stream OpenRead(string path);
+		Stream OpenWrite(string path);
+		Stream Open(string path, FileMode mode, FileAccess access, FileShare share = FileShare.None, FileOptions fileOptions = FileOptions.None);
+		IEnumerable<string> EnumerateFiles(string path, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly);
+		IEnumerable<string> EnumerateDirectories(string path, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly);
+		void Copy(string sourceFileName, string destinationFileName, bool overwrite = false);
+		Task CopyAsync(string sourceFileName, string destinationFileName, bool overwrite = false);
+		void Move(string sourceFileName, string destinationFileName);
+		void DeleteFile(string path);
+		void SetAttributes(string path, FileAttributes fileAttributes);
+		void Encrypt(string path);
+
+		DateTime GetLastWriteTimeUtc(string path);
+		void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc);
+	}
+}
